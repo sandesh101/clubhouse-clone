5b3d61ce7d06b44db026cb55479500cb4dfef991
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-12-13 22:59:09 +0545

    Fix: One container of room

--- lib/Widgets/commingrooms.dart
+++ lib/Widgets/commingrooms.dart
@@ -1,15 +1,47 @@
+import 'dart:convert';
+
 import 'package:clubhouse_clone/Variables/variables.dart';
 import 'package:flutter/material.dart';
 
-class CommingRooms extends StatelessWidget {
+class CommingRooms extends StatefulWidget {
+  @override
+  _CommingRoomsState createState() => _CommingRoomsState();
+}
+
+class _CommingRoomsState extends State<CommingRooms> {
+  Widget roomDetails(
+    String roomTitle,
+    String roomName,
+    String fpName,
+    String spName,
+    String tpName,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Variables.rooms,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Text(
+              roomTitle,
+              style: const TextStyle(fontSize: 15, color: Colors.black),
+            ),
+            Text(roomName),
+            Text(fpName),
+            Text(spName),
+            Text(tpName),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Variables.rooms,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Text('This is container'),
-    );
+    return roomDetails('INVESTOR FOROUM', 'NEPSE Upcoming Scenario',
+        'Sandesh Rimal', 'Kumar Sanu', 'Sanam Slathia');
   }
 }
